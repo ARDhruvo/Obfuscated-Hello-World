03fb2de7c408e2215bd23f86f7cf320faa9ddb69
ARDhruvo <138615515+ARDhruvo@users.noreply.github.com>
2024-03-27 19:05:23 +0600

    Pika Pika

---
+++ Hello World.c
@@ -0,0 +1,22 @@
+/*
+ * @author Kashikizu/ARDhruvo
+ * File Creation Date: 27/03/2024 (dd/mm/yyyy)
+ */
+
+#include <stdio.h>
+#define pika int
+#define Pikachu main
+#define pikapika (
+#define pipika )
+#define Pika {
+#define pikachu }
+#define PiPikachu printf
+#define Pikapika return
+#define piika 0
+#define chu ;
+
+
+pika Pikachu pikapika pipika Pika
+    printf pikapika "Hello World" pipika chu
+    Pikapika piika chu
+pikachu
